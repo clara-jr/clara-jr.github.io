4aefd3d083e17ab3a8202062eb78740b274ce0b6
clara-jr <clarajimenezrecio@gmail.com>
2020-08-04 20:42:46 +0200

    Updates

---
+++ precache-manifest.f713501e9b51609255d62a39bdb041ba.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd4f75495ab2482b981d019b0cfacfb6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e6fcf1454a8230738f4d",
+    "url": "/static/css/2.d7549341.chunk.css"
+  },
+  {
+    "revision": "16be3daa48ff17089660",
+    "url": "/static/css/main.38c45220.chunk.css"
+  },
+  {
+    "revision": "e6fcf1454a8230738f4d",
+    "url": "/static/js/2.144005b0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.144005b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16be3daa48ff17089660",
+    "url": "/static/js/main.018dd286.chunk.js"
+  },
+  {
+    "revision": "44155d13e6a85ecf67ee",
+    "url": "/static/js/runtime-main.56b3efbf.js"
+  },
+  {
+    "revision": "2f95a3942a3b4ac69ae6d66d5bb1d1c3",
+    "url": "/static/media/CV-es.2f95a394.pdf"
+  },
+  {
+    "revision": "3ca905f2d84214ff5a71cdf3b119682b",
+    "url": "/static/media/IBMPlexMono-Text.3ca905f2.otf"
+  },
+  {
+    "revision": "94425de920d9eaea4ea9c9e55a39487c",
+    "url": "/static/media/IBMPlexSans-Bold.94425de9.otf"
+  },
+  {
+    "revision": "eb4a9d475a5b2bf9bcd8cf4a860f4e67",
+    "url": "/static/media/IBMPlexSans-Text.eb4a9d47.otf"
+  },
+  {
+    "revision": "3ad6ff9a2915bff3ac150931cd62608c",
+    "url": "/static/media/IBMPlexSans-TextItalic.3ad6ff9a.otf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
