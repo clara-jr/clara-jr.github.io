d4d71fea2dc9c7db9bf8e6ecb99b57b3a34bc679
clara-jr <clarajimenezrecio@gmail.com>
2021-04-12 20:28:06 +0200

    use eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,48 +1,48 @@
 module.exports = {
-	'env': {
-		'browser': true,
-		'es6': true,
-		'node': true
-	},
-	'extends': [
-		'eslint:recommended',
-		'plugin:react/recommended'
-	],
-	'globals': {
-		'Atomics': 'readonly',
-		'SharedArrayBuffer': 'readonly'
-	},
-	'parserOptions': {
-		'ecmaFeatures': {
-			'jsx': true
-		},
-		'ecmaVersion': 2018,
-		'sourceType': 'module'
-	},
-	'plugins': [
-		'react'
-	],
-	'rules': {
+  'env': {
+    'browser': true,
+    'es6': true,
+    'node': true
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:react/recommended'
+  ],
+  'globals': {
+    'Atomics': 'readonly',
+    'SharedArrayBuffer': 'readonly'
+  },
+  'parserOptions': {
+    'ecmaFeatures': {
+      'jsx': true
+    },
+    'ecmaVersion': 2018,
+    'sourceType': 'module'
+  },
+  'plugins': [
+    'react'
+  ],
+  'rules': {
     'react/no-unescaped-entities': 'off',
     'react/react-in-jsx-scope': 'off',
     'react/prop-types': 'off',
     'no-undef': 'warn',
     'no-unused-vars': 'warn',
-		'indent': [
-			'error',
-			2
-		],
-		'linebreak-style': [
-			'error',
-			'unix'
-		],
-		'quotes': [
-			'error',
-			'single'
-		],
-		'semi': [
-			'error',
-			'never'
-		]
-	}
+    'indent': [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'never'
+    ]
+  }
 }
